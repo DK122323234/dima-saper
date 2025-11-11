c1c1e2ebaa4cd9a3494679714e00a996009d26c9
DK122323234 <dkamensky@licei10.ru>
2025-11-11 23:55:16 +0300

    Новый код

--- src/main/java/com/example/dimasapper/Field.java
+++ src/main/java/com/example/dimasapper/Field.java
@@ -2,101 +2,101 @@ package com.example.dimasapper;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 public class Field {
-    public List<Integer> returnField(List<Integer> list){
-       List<Integer> list1 = new ArrayList<>();
-
-        int i = 81 - list.size();
-        while(i > 0){
-            list.add(0);
-            i--;
-        }
-        int a = 0;
-        for (Integer integer : list) {
-
-            if (integer == 0) {
-                try {
-                    if (list.get(a - 1).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                try {
-                    if (list.get(a + 1).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                try {
-                    if (list.get(a - 9).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                try {
-                    if (list.get(a + 9).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                try {
-                    if (list.get(a - 8).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                try {
-                    if (list.get(a - 10).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                try {
-                    if (list.get(a + 10).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-
-                try {
-                    if (list.get(a + 8).equals(9)) {
-                        integer = integer + 1;
-                    }
-                }
-                catch (Exception e) {
-                    System.out.println(e);
-                }
-                System.out.println(integer);
-                 list1.add(integer);
-        }
-            if(integer == 9){
+    public List<Integer> returnField(Set<Integer> set){
+        List<Integer> list1 = new ArrayList<>();
+        int i = 0;
+           while(list1.size() < 81){
+                 if (set.contains(i)){
                 list1.add(9);
-            }
 
-            System.out.println(integer);
-            a++;
+            }
+            else {
+                list1.add(0);
+            }
+            i++;
         }
+           i = 0;
+
+
+           for (Integer integer : list1){
+               if (integer == 9){
+                   try {
+                     if (i != 0 && i != 9 && i != 18 && i != 27 && i != 36 && i != 45
+                             && i != 54 && i != 63 && i != 72){
+                         if (list1.get(i - 1) != 9){
+                             list1.set(i - 1, list1.get(i - 1) + 1);
+
+                         }
+                         if (i + 8 <= 80) {
+                             if (list1.get(i + 8) != 9) {
+                                 list1.set(i + 8, list1.get(i + 8) + 1);
+                             }
+                         }
+                       }
+                     else {
+                         System.out.println("Если это читает Лёша, перестань немедлено");
+                     }
+
+
+
+
+
+                       if (i != 8 && i != 17 && i  != 26 && i != 35 && i != 44 &&
+                                i != 53 && i != 62 && i != 71 && i != 80){
+
+                           if (list1.get(i + 1) != 9) {
+                               list1.set(i + 1, list1.get(i + 1) + 1);
+                           }
+                           if (i - 10 >= 0) {
+                               if (list1.get(i - 10) != 9) {
+                                   list1.set(i - 10, list1.get(i - 10) + 1);
+                               }
+                           }
+
+
+                       }
+                       else {
+                           System.out.println("Если это читает Лёша, Я ЖЕ СКАЗАЛ ПЕРЕСТАТЬ");
+
+                       }
+                   }
+                   catch (Exception e){
+                       System.out.println("ПРОЧИТАТЬ:");
+                   }
+
+                       if (i - 9 >= 0) {
+                           if (list1.get(i - 9) != 9) {
+                           list1.set(i - 9, list1.get(i - 9) + 1);
+                       }
+                   }
+
+                       if (i + 9 <= 80) {
+                           if (list1.get(i + 9) != 9) {
+                           list1.set(i + 9, list1.get(i + 9) + 1);
+                       }
+                   }
+
+                   if (i - 8 >= 0) {
+                       if (list1.get(i - 8) != 9) {
+                           list1.set(i - 8, list1.get(i - 8) + 1);
+                       }
+                   }
+
+
+                   if (i + 10 <= 80) {
+                       if (list1.get(i + 10) != 9) {
+                           list1.set(i + 10, list1.get(i + 10) + 1);
+                       }
+                   }
+
+               }
+               i ++;
+           }
+
         return list1;
     }
 }
+
--- src/main/java/com/example/dimasapper/MinePlacement.java
+++ src/main/java/com/example/dimasapper/MinePlacement.java
@@ -1,40 +1,19 @@
 package com.example.dimasapper;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-import java.util.stream.Stream;
+import java.util.*;
+import java.util.stream.IntStream;
 
 public class MinePlacement {
-    public List<Integer> returnMines() {
-        List<Integer> mines = new ArrayList<Integer>();
-        int a = 0;
-        while (a <= 8) {
+    public Set<Integer> returnMines() {
+        Set<Integer> set = new HashSet<>();
+        Random rand = new Random();
+        IntStream.of(rand.nextInt(80), rand.nextInt(80), rand.nextInt(80), rand.nextInt(80), rand.nextInt(80), rand.nextInt(80),
+                rand.nextInt(80), rand.nextInt(80)).forEach(set::add);
 
-            if (mines.size() >= 80) {
-                if (a < 8 ){
-
-                    mines.subList(0, a).clear();
-
-                 Integer f = Integer.valueOf(Math.toIntExact(Stream.of(mines).filter(value -> value.equals(9)).count())) ;
-                    if (a > f){
-                        for (int i = 0; i < 8-f; i++) {
-                              mines.add(9);
-                        }
-                    }
-                }
-            }
-
-            Random rand = new Random();
-            if (rand.nextInt(100) <= 10 + a) {
-                mines.add(9);
-                a++;
-            }
-            else {
-                mines.add(0);
-            }
-
+        while (set.size() < 8) {
+            set.add(rand.nextInt(80));
         }
-        return mines;
+
+        return set;
     }
 }
